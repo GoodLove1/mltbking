d49607740afe02f8c6262018d03297c28ffc1cc5
kofhb <117967016+kofhb@users.noreply.github.com>
2022-12-26 21:40:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,1 @@
-#python3 update.py && python3 -m bot
-
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/kofhb/mltbking.git /mltbking
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /mltbking
-fi
-cd /mltbking
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 update.py
+python3 update.py && python3 -m bot
